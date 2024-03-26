30176265644675e1b3e4bdb3ec2378d21b0d2084
unknown <mo7md28@gmail.com>
2024-03-26 15:35:09 +0200

    add new section

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,17 +1,34 @@
-
-import './hreo.css';
+import "./hreo.css";
 const Hero = () => {
-    return (
-        <section className='hero '>
-            <div className="left-sec border">
-                <div className="parent-avatar">
-                    <img src="/public/avatar-modified.png" alt=""  className='avatar'/>
-                    <span className='icon-verified'></span>
-                </div>
-            </div>
-            <div className="right-sec animation border">right</div>
-        </section>
-    );
-}
+  return (
+    <section className="hero ">
+      <div className="left-sec ">
+        <div >
+          <div className="parent-avatar">
+            <img src="/public/avatar-modified.png" alt="" className="avatar" />
+            <span className="icon-verified"/>
+          </div>
+          <div className="avatar-desc">
+            <h2>Software designer, founder adn amateur astronaut.</h2>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere,
+              expedita ipsam ipsum consequuntur amet, asperiores autem quidem
+              officiis voluptatum dicta corrupti, alias earum commodi quae
+              repellat cupiditate quisquam saepe? Rerum.
+            </p>
+          </div>
+          <div className="icons-container">
+            <span className="icon-x"/>
+            <span className="icon-instagram"/>
+            <span className="icon-whatsapp"/>
+            <span className="icon-github"/>
+            <span className="icon-linkedin"/>
+          </div>
+        </div>
+      </div>
+      <div className="right-sec animation border">right</div>
+    </section>
+  );
+};
 
 export default Hero;
--- src/components/maincont/MainContent.jsx
+++ src/components/maincont/MainContent.jsx
@@ -1,11 +1,84 @@
-import React from 'react';
-import './mainCont.css';
+import React from "react";
+import "./mainCont.css";
 const MainContent = () => {
-    return (
-        <div>
-            Mian Contanet
+  return (
+    <section className="main-container">
+      <aside className="btn-container flex ">
+        <button className="btn active">
+          <i className="animation"></i>All Projects<i className="animation"></i>
+        </button>
+        <button className="btn">
+          <i className="animation"></i>HTML & CSS<i className="animation"></i>
+        </button>
+        <button className="btn">
+          <i className="animation"></i>javaScript<i className="animation"></i>
+        </button>
+        <button className="btn">
+          <i className="animation"></i>react<i className="animation"></i>
+        </button>
+        <button className="btn">
+          <i className="animation"></i>flluter<i className="animation"></i>
+        </button>
+      </aside>
+
+      <div className=" main">
+        <div className="cards-container">
+            <div className="card ">
+                <img src="/public/1.jpg" alt="" />
+                <div className="card-box">
+                    <h3 className="card-title">title</h3>
+                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur.</p>
+                    <div className="flex">
+                        <span className="icon-link"></span>
+                        <span className="icon-github"></span>
+                        <button className="">more <span className="icon-arrow-right"/></button>
+                    </div>
+                </div>
+            </div>
+            <div className="card">
+                <img src="/public/1.jpg" alt="" />
+                <div className="card-box">
+                    <h3 className="card-title">title</h3>
+                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur.</p>
+                    <div className="flex">
+                        <span className="icon-link"></span>
+                        <span className="icon-github"></span>
+                        <button className="">more <span className="icon-arrow-right"/></button>
+                    </div>
+                </div>
+            </div>
+            <div className="card">
+                <img src="/public/1.jpg" alt="" />
+                <div className="card-box">
+                    <h3 className="card-title">title</h3>
+                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur.</p>
+                    <div className="flex">
+                        <span className="icon-link"></span>
+                        <span className="icon-github"></span>
+                        <button className="">more <span className="icon-arrow-right"/></button>
+                    </div>
+                </div>
+            </div>
+            <div className="card">
+                <img src="/public/1.jpg" alt="" />
+                <div className="card-box">
+                    <h3 className="card-title">title</h3>
+                    <p className="card-text">Lorem ipsum dolor sit amet, consectetur.</p>
+                    <div className="flex">
+                        <span className="icon-link"></span>
+                        <span className="icon-github"></span>
+                        <button className="">more <span className="icon-arrow-right"/></button>
+                    </div>
+                </div>
+            </div>
         </div>
-    );
-}
+        
+        
+        
+      </div>
+      
+    </section>
+  );
+};
 
 export default MainContent;
